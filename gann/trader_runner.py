4ec5701da77099de72e405880602bf49ff3575e4
Clemens Manert <Clemens@Manert.de>
2022-01-03 06:14:09 +0100

    Remove redundant logger config

--- gann/trader_runner.py
+++ gann/trader_runner.py
@@ -3,19 +3,9 @@ import sys
 
 class TraderRunner:
     """ Runs a trader and prints trading activity."""
-    def __init__(self, traders=None, depots=None, log=sys.stdout):
+    def __init__(self, traders=None, depots=None):
         self.traders = traders if traders is not None else list()
         self.depots = depots if depots is not None else list()
-
-        handler = logging.StreamHandler(log)
-        handler.setFormatter(logging.Formatter(
-            fmt='%(asctime)s %(levelname)-8s %(message)s',
-            datefmt='%Y-%m-%d %H:%M:%S')
-                             )
-
-        logging.getLogger().addHandler(handler)
-
-        logging.getLogger().setLevel(logging.INFO)
 
     def add_order(self, offer):
         """Progresses a given order"""
